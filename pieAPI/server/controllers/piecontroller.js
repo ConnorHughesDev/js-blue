e1bf6de589ad0f3f176ed92537574ba803fa41c8
Connor Hughes <cj.hughes.dev@gmail.com>
2021-11-24 17:51:39 -0500

    first commit

---
+++ pieAPI/server/controllers/piecontroller.js
@@ -0,0 +1,117 @@
+// const Express = require("express")
+// const router = Express.Router()
+const { PieModel } = require("../models")
+
+const router = require("express").Router()
+
+//! GET ALL
+router.get("/", async (req, res) => {
+    // async function because sequelize methods generally return a promise.
+    try {
+        // holds a promise resolved info from .findAll() method of sqlize
+        const allPies = await PieModel.findAll()
+        // the response is sent with a status and instance of info that's turned into an object
+        res.status(200).json(allPies)
+    } catch (err) {
+        // send a response with an error status code and an object showing the error
+        res.status(500).json({
+            error: err,
+        })
+    }
+})
+
+//! CREATE ONE
+router.post("/", async (req, res) => {
+
+    // object destructuring the values of the body of incoming request
+    const {
+        nameOfPie,
+        baseOfPie,
+        crust,
+        timeToBake,
+        servings,
+        rating
+    } = req.body
+
+    console.log(nameOfPie, baseOfPie, "----------------")
+
+    try {
+        const Pie = await PieModel.create({
+            nameOfPie,
+            baseOfPie,
+            crust,
+            timeToBake,
+            servings,
+            rating,
+        })
+
+        res.status(201).json({
+            message: "Pie successfully created.",
+            Pie,
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: `Failed to create pie: ${err}`
+        })
+    }
+})
+
+//! GET ONE
+router.get("/:name", async (req, res) => {
+    try {
+        const locatedPie = await PieModel.findOne({
+            where: {
+                nameOfPie: req.params.name,
+            },
+        })
+        res.status(200).json({
+            message: "Pie successfully retrieved",
+            locatedPie,
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: `Failed to retrieve pies: ${err}`
+        })
+    }
+})
+
+//! UPDATE ONE
+// router.put("/:id", async (req, res) => {
+//     const {
+//         nameOfPie,
+//         baseOfPie,
+//         crust,
+//         timeToBake,
+//         servings,
+//         rating
+//     } = req.body
+
+//     try {
+//         PieModel.update(
+//             { nameOfPie, baseOfPie, crust, timeToBake, servings, rating},
+//             { where: { id: req.params.id }, returning: true }
+//         )
+//     }
+// })
+
+//! DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+        await PieModel.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then((result) => {
+            res.status(200).json({
+                message: "Pie successfully deleted",
+                deletedPie: result
+            })
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: `Failed to delete pie: ${err}` 
+        })
+    }
+})
+
+module.exports = router
